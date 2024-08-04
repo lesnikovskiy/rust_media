30b440ab4860645730656e84ae7e70c9ce6c3e40
lesnikovskiy <lesnikovski@gmail.com>
2024-08-04 23:01:49 +0300

    Use shorter way to import modules through use content::

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,30 @@
 mod content;
 
-fn main() {
-    let audiobook = content::media::Media::Audiobook { title: String::from("An Audiobook") };
+use content::catalog::Catalog;
+use content::media::Media;
 
-    let good_movie = content::media::Media::Movie {
+fn main() {
+    let audiobook = Media::Audiobook { title: String::from("An Audiobook") };
+
+    let good_movie = Media::Movie {
         title: String::from("Good Moview"),
         director: String::from("Director"),
     };
 
-    let bad_book = content::media::Media::Book {
+    let bad_book = Media::Book {
         title: String::from("Bad Book"),
         author: String::from("Bad Author"),
     };
 
-    let podcast = content::media::Media::Podcast(135);
+    let podcast = Media::Podcast(135);
 
-    let placeholder = content::media::Media::Placeholder;
+    let placeholder = Media::Placeholder;
 
     // println!("{}", audiobook.description());
     // println!("{}", good_movie.description());
     // println!("{}", bad_book.description());
 
-    let mut catalog = content::catalog::Catalog::new();
+    let mut catalog = Catalog::new();
 
     catalog.add(audiobook);
     catalog.add(good_movie);
@@ -29,7 +32,7 @@ fn main() {
     catalog.add(podcast);
     catalog.add(placeholder);
 
-    let placeholder = content::media::Media::Placeholder;
+    let placeholder = Media::Placeholder;
     let item = catalog.get_by_index(10).unwrap_or(&placeholder);
     println!("Item: {:#?}", item);
 }
